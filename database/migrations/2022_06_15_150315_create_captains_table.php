4bdb39d5ef568cd119b92e3980513156f2d2ea3f
jacmill <jakub3020.jmy@gmail.com>
2022-06-26 01:05:28 +0200

    Added remember token to captain migration

--- database/migrations/2022_06_15_150315_create_captains_table.php
+++ database/migrations/2022_06_15_150315_create_captains_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('last_name', 255);
             $table->string('email', 255);
             $table->string('password', 255);
+            $table->string('remember_token', 255);
         });
     }
 
