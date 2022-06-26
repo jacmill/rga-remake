b263daa0039c76297f1aa61d39b338fb316be76d
jacmill <jakub3020.jmy@gmail.com>
2022-06-26 13:05:54 +0200

    Some minor changes and added displaying errors under their respective inputs

---
+++ lang/pl/validation.php
@@ -0,0 +1,11 @@
+<?php 
+    return [
+        'password' => [
+            'letters' => 'Hasło musi zawierać przynajmniej jedną literę.',
+            'mixed' => 'Hasło musi zawierać przynajmniej jedną dużą i małą literę.',
+            'numbers' => 'Hasło musi zawierać przynajmniej jedną cyfrę.',
+            'symbols' => 'Hasło musi zawierać przynajmniej jeden znak specjalny',
+        ],
+        'required' => 'Pole jest wymagane.',
+    ]
+?>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,35 +6,43 @@
         <label>
           <span>Imię</span>
           <input type="text" name="name" id="name" class="block">
+          @if ($errors->has('password')) <p class=" text-red-500 break-words text-sm"> {{ $errors->first('name') }}</p> @endif
         </label>
         <label>
           <span>Nazwisko</span>
           <input type="text" name="lastname" id="lastname" class="block">
+          @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('lastname') }}</p> @endif
         </label>
       </div>
       <label class="block">
         <span>Adres e-mail</span>
         <input type="text" name="email" id="email" class="block w-full">
+        @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('email') }}</p> @endif
       </label>
       <label class="block">
         <span>Hasło</span>
         <input type="password" name="password" id="password" class="block w-full">
+        @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('password') }}</p> @endif
       </label>
       <div class="flex gap-3 flex-wrap">
         <label>
           <span>Wiek</span>
           <input type="text" name="age" id="age" class="block">
+          @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('age') }}</p> @endif
         </label>
         <label>
           <span>Szkoła</span>
           <select name="school" id="school" class="block">
             <option value="">TEST</option>
           </select>
+          @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('school') }}</p> @endif
+
         </label>
       </div>
       <label class="block">
         <span>Nazwa drużyny</span>
         <input type="text" name="teamname" id="teamname" class="block w-full">
+        @if ($errors->has('password')) <p class=" text-red-500 break-words"> {{ $errors->first('teamname') }}</p> @endif
       </label>
       <label class="block">
         <span>Kilka słów o drużynie (opcjonalne)</span>
@@ -43,11 +51,4 @@
       <button type="submit">Zarejestruj</button>
     </form>
   </div>
-  @if ($errors->any())
-    <ul>
-      @foreach ($errors->all() as $error)
-        <li>{{ $error }}</li>
-      @endforeach
-    </ul>
-  @endif
 </x-layout>
