0812d97936253c321b2e570f1de6d99554797c3b
jacmill <jakub3020.jmy@gmail.com>
2022-06-28 00:08:41 +0200

    added logout button

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -4,21 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href= {{ asset("css/app.css") }} rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
-    <main>
-        <aside>
+    <main class="flex">
+        <aside >
             <h1>Panel kapitana</h1>
             <nav>
                 <ul>
                     <li><a href="/dashboard">Podsumowanie</a></li>
                     <li><a href="/dashboard/teammates">Zawodnicy</a></li>
                 </ul>
-                <div>
-                    <a href="/logout">Wyloguj się</a>
-                </div>
             </nav>
+            <div>
+                <a href="/logout">Wyloguj się</a>
+            </div>
         </aside>
         <section>
             {{ $slot }}
