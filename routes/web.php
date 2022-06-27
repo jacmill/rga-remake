a1c397312101c2ef8b882c87c262d1771fb6a7cc
jacmill <jakub3020.jmy@gmail.com>
2022-06-27 13:23:48 +0200

    Working authentication and dashboard route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Models\Team;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,4 +17,11 @@ Route::controller(LoginController::class)->group(function(){
 Route::controller(RegisterController::class)->group(function(){
     Route::get('register', 'show');
     Route::post('register', 'create');
+});
+
+Route::get('dashboard', function() {
+    $user = Auth::user();
+    $username = $user->name . " " . $user->last_name;
+    $team = DB::table('teams')->where('id', $user->team_id)->first()->name;
+    return view('dashboard.dashboard', ["username" => $username, "team" => $team]);
 });
