3241683856d4536cd9587ea54ccf7dfa257669cd
jacmill <jakub3020.jmy@gmail.com>
2022-06-30 00:11:02 +0200

    created method for reseting password

--- app/Http/Controllers/ResetPassword.php
+++ app/Http/Controllers/ResetPassword.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
-
+use Illuminate\Support\Str;
 
 class ResetPassword extends Controller
 {
     public function show_forgot_password() {
         return view("auth.forgot-password");
+    }
+    public function show_reset_password($token) {
+        return view('auth.reset-password', ["token" => $token]);
     }
     public function forgot_password(Request $request) {
         $request->validate([
@@ -22,5 +27,28 @@ class ResetPassword extends Controller
         return $status === Password::RESET_LINK_SENT
                         ? back()->with(["status" => __($status)])
                         : back()->withErrors(["email" => __($status)]);
+    }
+    public function reset_password(Request $request) {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function ($user, $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+     
+                $user->save();
+     
+                event(new PasswordReset($user));
+            }
+        );
+     
+        return $status === Password::PASSWORD_RESET
+                    ? redirect()->route('login')->with('status', __($status))
+                    : back()->withErrors(['email' => [__($status)]]);
     }
 }
