7eea3986b6bd262aae7f8af6ce549cf01f2e21fb
jacmill <jakub3020.jmy@gmail.com>
2022-07-09 00:38:26 +0200

    Created teammate model and migration

---
+++ database/migrations/2022_07_08_223500_create_teammates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teammates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('last_name', 255);
+            $table->string('email', 255);
+            $table->string('school', 255);
+            $table->foreignId('team_id')->constrained('teams');
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teammates');
+    }
+};
