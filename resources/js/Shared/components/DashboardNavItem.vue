1d73dabf303dba544e92ee9f7dc3eae7cb17d208
jacmill <jakub3020.jmy@gmail.com>
2022-07-10 00:26:30 +0200

    Add dashboard nav item component

---
+++ resources/js/Shared/components/DashboardNavItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full text-zinc-300 p-2">
+        <Link href="{{ to }}"><slot/></Link>
+    </div>
+</template>
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+const props = defineProps(["to"]);
+</script>
