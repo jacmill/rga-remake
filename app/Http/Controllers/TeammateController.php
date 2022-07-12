265b45e11fd8b5323aa018ef156a57412aaa294e
jacmill <jakub3020.jmy@gmail.com>
2022-07-12 14:06:19 +0200

    Created teammate resource controller

---
+++ app/Http/Controllers/TeammateController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teammate;
+use Illuminate\Http\Request;
+
+class TeammateController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Teammate  $teammate
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Teammate $teammate)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Teammate  $teammate
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Teammate $teammate)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Teammate  $teammate
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Teammate $teammate)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Teammate  $teammate
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Teammate $teammate)
+    {
+        //
+    }
+}
