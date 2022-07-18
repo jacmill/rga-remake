c8166fdc841fff00a4ead57d8e96efb0143e5dbb
jacmill <jakub3020.jmy@gmail.com>
2022-07-19 00:08:28 +0200

    some styling for avatar on home page

--- resources/js/Shared/HomeLayout.vue
+++ resources/js/Shared/HomeLayout.vue
@@ -1,9 +1,11 @@
 <template>
     <header class="flex gap-3 justify-evenly items-center bg-zinc-800 text-white py-2">
         <Link href="/"><img src="../../images/logo.svg" alt="logo" class=" w-36"></Link>
-        <Link v-if="team !== null" href="/dashboard" class="flex">
+        <Link v-if="team !== null" href="/dashboard" 
+                    class="flex items-center gap-3 hover:bg-zinc-600 transition-all py-1 px-2 rounded-lg">
             {{ team.name }} 
-            <img :src="`/team_logos/${team.logo}`" class="w=5"/>
+            <img :src="`/team_logos/${team.logo}`" 
+                class="w-10 h-10 object-cover rounded-lg border-blue-500 border-2"/>
         </Link>
         <Nav v-else />
     </header>
