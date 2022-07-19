58f1d5572aff3fa31f6046342d988b4bcfc9c539
jacmill <jakub3020.jmy@gmail.com>
2022-07-20 00:50:08 +0200

    Got working counter

--- resources/js/Shared/components/Countdown.vue
+++ resources/js/Shared/components/Countdown.vue
@@ -1,36 +1,48 @@
 <template>
     <div class="flex gap-3">
         <div>
-            <p>{{ displayDays }}</p>
+            <p>{{ display.days }}</p>
             <p>Dni</p>
         </div>
         <div>
-            <p>{{ displayHours }}</p>
+            <p>{{ display.hours }}</p>
             <p>Godzin</p>
         </div>    
         <div>
-            <p>{{ displayMinutes }}</p>
+            <p>{{ display.minutes }}</p>
             <p>Minut</p>
         </div>    
         <div>
-            <p>{{ displaySeconds }}</p>
+            <p>{{ display.seconds }}</p>
             <p>Sekund</p>
         </div>    
     </div>
 </template>
 <script setup>
-    const targetDate = new Date("2022-09-09T09:00:00:00").getTime();
+    import { ref } from '@vue/reactivity';
+    import { onMounted } from 'vue';
 
-    const displaySeconds = 0;
-    const displayMinutes = 0;
-    const displayHours = 0;
-    const displayDays = 0;
-    
-    const _seconds = _ => 1000;
-    const _minutes = _ => _seconds * 60;
-    const _hours = _ => _minutes * 60;
-    const _days = _ => _days * 24;
+    const launchDate = new Date("August 5, 2022 09:00:00");
+    const display = {
+        seconds: ref(0),
+        minutes: ref(0),
+        hours: ref(0),
+        days: ref(0)
+    };
+    const _seconds = 1000;
+    const _minutes = _seconds * 60;
+    const _hours = _minutes * 60;
+    const _days = _hours * 24
+    const showRemaining = setInterval(() => {
+        const currentDate = new Date();
+        const distance = launchDate.getTime() - currentDate.getTime();
 
-    const formatTime = time => time < 10 && `0${time}`
-
+        display.seconds.value = Math.floor((distance % _minutes) / _seconds);
+        display.minutes.value = Math.floor((distance  % _hours) / _minutes);
+        display.hours.value = Math.floor((distance  % _days) / _hours);
+        display.days.value = Math.floor(distance / _days);
+    }, 1000);
+    onMounted(() => {
+       showRemaining
+    });
 </script>
