1beadef313aead3a17743e9e58a5b6bf60346f34
jacmill <jakub3020.jmy@gmail.com>
2022-07-20 00:00:48 +0200

    Added helper function for formating time

--- resources/js/Shared/components/Countdown.vue
+++ resources/js/Shared/components/Countdown.vue
@@ -19,10 +19,18 @@
     </div>
 </template>
 <script setup>
+    const targetDate = new Date("2022-09-09T09:00:00:00").getTime();
+
     const displaySeconds = 0;
     const displayMinutes = 0;
     const displayHours = 0;
     const displayDays = 0;
-
     
+    const _seconds = _ => 1000;
+    const _minutes = _ => _seconds * 60;
+    const _hours = _ => _minutes * 60;
+    const _days = _ => _days * 24;
+
+    const formatTime = time => time < 10 && `0${time}`
+
 </script>
