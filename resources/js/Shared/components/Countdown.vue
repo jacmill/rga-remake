8d3558820b589abe5db605b14aa8968d660378a9
jacmill <jakub3020.jmy@gmail.com>
2022-07-20 16:29:18 +0200

    Changed display object to Map)

--- resources/js/Shared/components/Countdown.vue
+++ resources/js/Shared/components/Countdown.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="flex gap-3">
         <div>
-            <p>{{ display.days }}</p>
+            <p>{{ display.get("days") }}</p>
             <p>Dni</p>
         </div>
         <div>
-            <p>{{ display.hours }}</p>
+            <p>{{ display.get("hours") }}</p>
             <p>Godzin</p>
         </div>    
         <div>
-            <p>{{ display.minutes }}</p>
+            <p>{{ display.get("minutes") }}</p>
             <p>Minut</p>
         </div>    
         <div>
-            <p>{{ display.seconds }}</p>
+            <p>{{ display.get("seconds") }}</p>
             <p>Sekund</p>
         </div>    
     </div>
@@ -23,12 +23,12 @@
     import { onMounted } from 'vue';
 
     const launchDate = new Date("August 5, 2022 09:00:00");
-    const display = {
-        seconds: ref(0),
-        minutes: ref(0),
-        hours: ref(0),
-        days: ref(0)
-    };
+    const display = new Map([
+        ["seconds", ref(0)],
+        ["minutes", ref(0)],
+        ["hours", ref(0)],
+        ["days", ref(0)]
+    ]);
     const _seconds = 1000;
     const _minutes = _seconds * 60;
     const _hours = _minutes * 60;
@@ -36,11 +36,11 @@
     const showRemaining = setInterval(() => {
         const currentDate = new Date();
         const distance = launchDate.getTime() - currentDate.getTime();
-
-        display.seconds.value = Math.floor((distance % _minutes) / _seconds);
-        display.minutes.value = Math.floor((distance  % _hours) / _minutes);
-        display.hours.value = Math.floor((distance  % _days) / _hours);
-        display.days.value = Math.floor(distance / _days);
+        
+        display.get("seconds").value = Math.floor((distance % _minutes) / _seconds);
+        display.get("minutes").value = Math.floor((distance  % _hours) / _minutes);
+        display.get("hours").value = Math.floor((distance  % _days) / _hours);
+        display.get("days").value = Math.floor(distance / _days);
     }, 1000);
     onMounted(() => {
        showRemaining
