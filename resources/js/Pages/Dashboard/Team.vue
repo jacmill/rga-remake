6dc1f52fe8909720d3e8bf769045f8797f19c6cb
jacmill <jakub3020.jmy@gmail.com>
2022-07-22 00:38:03 +0200

    Created image preview component for team form

--- resources/js/Pages/Dashboard/Team.vue
+++ resources/js/Pages/Dashboard/Team.vue
@@ -1,22 +1,13 @@
 <template>
     <DashboardLayout title="Zespół">
         <form @submit.prevent="submit">
-            <label class="block">Logo drużyny</label>
-            <div class="flex">
-                <img :src="`/team_logos/${logo}`" alt="logo" class="w-1/2">
-                <div>
-                    <input type="file" @input="form.logo = $event.target.files[0]" />
-                    <progress v-if="form.progress" :value="form.progress.percentage" max="100">
-                      {{ form.progress.percentage }}%
-                    </progress>
-                </div>
-            </div>
+            <ImagePreview />
             <button type="submit">Ustaw</button>
         </form>
     </DashboardLayout>
 </template>
 <script setup>
-    import { Inertia } from '@inertiajs/inertia';
+    import ImagePreview from '../../Shared/components/Forms/ImagePreview.vue';
     import { useForm, usePage } from '@inertiajs/inertia-vue3';
     import DashboardLayout from '../../Shared/DashboardLayout.vue';
     const logo = usePage().props.value.auth.team.logo;
---
+++ resources/js/Shared/components/Forms/ImagePreview.vue
@@ -0,0 +1,10 @@
+<template>
+    <label class="block">Logo drużyny</label>
+    <div class="grid">
+        <label>Prześlij swoje logo</label>
+        <input type="file"
+        >
+    </div>
+</template>
+<script setup>
+</script>
